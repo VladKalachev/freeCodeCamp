8ff8c2fa0896563d0c956c3ab0c175b5fcd225eb
Corvax <vlvete1990@yandex.ru>
2021-09-01 22:37:15 +0300

    chore: added express

--- node2018/app.js
+++ node2018/app.js
@@ -1,3 +1,9 @@
-const _ = require('lodash');
-let example = _.fill([11, 32,3,2], "banana", 1, 4);
-console.log(example);
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+app.listen(3000);

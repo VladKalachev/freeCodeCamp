7f12ca2c661739a27ebef199e375902b3478e08d
Corvax <vlvete1990@yandex.ru>
2021-09-01 22:41:24 +0300

    chore: express params

--- node2018/app.js
+++ node2018/app.js
@@ -6,4 +6,14 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 })
 
+app.get('/example', (req, res) => {
+    res.send('example router');
+})
+
+app.get('/example/:name/:age', (req, res) => {
+    console.log(req.params);
+    const { name, age } = req.params;
+    res.send(`${name}: ${age}`);
+})
+
 app.listen(3000);

8bd35868f4cda5299c42f5fad0624a25a6c1ab3b
Corvax <vlvete1990@yandex.ru>
2021-09-01 20:56:31 +0300

    chore: created folde

--- node2018/app.js
+++ node2018/app.js
@@ -1,36 +1,8 @@
 const fs = require('fs');
-// create a file
-// fs.writeFile('example.txt','this is an example', (err) => {
-//     if(err)
-//         console.log(err);
-//     else {
-//          console.log('File successfully created');
-//          fs.readFile('example.txt', 'utf8', (err, file) => {
-//              if(err)
-//                 console.log(err)
-//             else 
-//                 console.log(file)
-//          })
-//     }   
-// })
-
-// fs.rename('example.txt', 'example2.txt', (err) => {
-//     if(err)
-//         console.log(err);
-//     else
-//         console.log('successfule file renamed')
-// })
-
-// fs.appendFile('example2.txt', 'Some data being appended', (err) => {
-//     if (err)
-//         console.log(err);
-//     else
-//         console.log('successfule file appended')  
-// })
-
-fs.unlink('example2.txt', (err) => {
-    if(err)
-        console.log(err);
-    else 
-        console.log('successfully deleted the file')
+fs.mkdir('tutorial', (err) => {
+    if (err) 
+        console.log(err)
+    else {
+        console.log('folder successfully created')
+    }
 })

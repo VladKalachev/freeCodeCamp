4ec1860c9adbde974a4aa2b61700949af48a5e84
Corvax <vlvete1990@yandex.ru>
2021-09-01 21:19:02 +0300

    chore: create read stream file

--- node2018/app.js
+++ node2018/app.js
@@ -1,43 +1,5 @@
 const fs = require('fs');
-// fs.mkdir('tutorial', (err) => {
-//     if (err) 
-//         console.log(err)
-//     else {
-//         fs.writeFile('./tutorial/example.txt', '123', (err) => {
-//             if(err)
-//                 console.log(err);
-//             else 
-//                 console.log('successfully created a file');
-//         })
-//     }
-// })
-
-// fs.unlink('./tutorial/example.txt', (err) => {
-//     if(err) {
-//         console.log(err);
-//     } else 
-//         console.log('deleted file');
-// })
-// fs.rmdir('tutorial', (err) => {
-//     if(err) {
-//         console.log(err);
-//     } else 
-//         console.log('deleted folder');
-// })
-
-fs.readdir('example', (err, files) => {
-    if (err)
-        console.log(err)
-    else {
-       for(let file of files) {
-           fs.unlink('./example/' + file, (err) => {
-               if(err) {
-                   console.log(err);
-               } 
-               else {
-                   console.log('successfully deleted file');
-               }
-           })
-       }
-    }
-})
+const readStream = fs.createReadStream('./example.txt', 'utf8');
+readStream.on('data', (chunk) => {
+    console.log(chunk);
+});

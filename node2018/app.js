4d2f2490dfecc329bf267fdd687c28365f286fd4
Corvax <vlvete1990@yandex.ru>
2021-09-01 21:08:02 +0300

    chore: delete files

--- node2018/app.js
+++ node2018/app.js
@@ -1,13 +1,26 @@
 const fs = require('fs');
-fs.mkdir('tutorial', (err) => {
-    if (err) 
-        console.log(err)
-    else {
-        fs.writeFile('./tutorial/example.txt', '123', (err) => {
-            if(err)
-                console.log(err);
-            else 
-                console.log('successfully created a file');
-        })
-    }
+// fs.mkdir('tutorial', (err) => {
+//     if (err) 
+//         console.log(err)
+//     else {
+//         fs.writeFile('./tutorial/example.txt', '123', (err) => {
+//             if(err)
+//                 console.log(err);
+//             else 
+//                 console.log('successfully created a file');
+//         })
+//     }
+// })
+
+fs.unlink('./tutorial/example.txt', (err) => {
+    if(err) {
+        console.log(err);
+    } else 
+        console.log('deleted file');
+})
+fs.rmdir('tutorial', (err) => {
+    if(err) {
+        console.log(err);
+    } else 
+        console.log('deleted folder');
 })

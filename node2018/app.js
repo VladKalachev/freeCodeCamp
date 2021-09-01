1a5d91249768c6c4479d58a25be51749ea8cd5f1
Corvax <vlvete1990@yandex.ru>
2021-09-01 21:32:30 +0300

    chore: Gzip Gunzip file

--- node2018/app.js
+++ node2018/app.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
-const readStream = fs.createReadStream('./example.txt', 'utf8');
-const writeStream = fs.createWriteStream('example2.txt');
+const zlib = require('zlib');
+const gzip = zlib.createGzip();
+const gunzip = zlib.createGunzip();
+const readStream = fs.createReadStream('./example2.txt.gz');
+const writeStream = fs.createWriteStream('uncompressed.txt');
 // readStream.on('data', (chunk) => {
 //     writeStream.write(chunk);
 // });
-readStream.pipe(writeStream);
+readStream.pipe(gunzip).pipe(writeStream);

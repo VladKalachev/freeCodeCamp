6d977a7269776e684e31e81d160ff48d2a75fafe
Corvax <vlvete1990@yandex.ru>
2021-09-02 21:26:51 +0300

    chore: added body-parser

--- node2018/app.js
+++ node2018/app.js
@@ -1,20 +1,12 @@
-
 const express = require('express');
+const bodyparser = require('body-parser');
 const path = require('path');
 const app = express();
-// app.use('/public', express.static(path.join(__dirname, 'static')));
-app.use(express.static(__dirname + '../public'));
-app.set("view engine", "ejs");
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'static', 'index.html'));
-// })
-app.get('/:userQuery', (req, res) => {
-    res.render('index', {
-        title: 'Title',
-        data: {
-            userQuery: req.params.userQuery
-        }
-    });
+
+app.use(bodyparser.json());
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
 })
 
 app.listen(3000);
